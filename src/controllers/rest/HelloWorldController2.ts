072e2be04bcab85392a5454a06cc24bcd502ca3d
Ethan Calvert <ecalvert@freightview.com>
2022-12-20 16:19:50 -0600

    init

---
+++ src/controllers/rest/HelloWorldController2.ts
@@ -0,0 +1,48 @@
+import {Controller} from "@tsed/di";
+import { OnSerialize } from "@tsed/json-mapper";
+import {CollectionOf, Generics, Get, Property, Returns} from "@tsed/schema";
+
+@Generics('T')
+class PagedModel<T> {
+  @CollectionOf('T')
+  data: T[]
+}
+
+class PeopleResponse {
+  @Property()
+  id: string
+
+  @OnSerialize(v => v?.toUpperCase())
+  name: string
+}
+
+class StrictPageModel {
+  @CollectionOf(PeopleResponse)
+  data: PeopleResponse[]
+}
+
+@Controller("/hello-world")
+export class HelloWorldController {
+  @Get("/bad")
+  @Returns(200, PagedModel<PeopleResponse>)
+  get() {
+    return {
+      data: [{
+        id: 'foo',
+        name: 'bar'
+      }]
+    }
+  }
+
+  @Get("/good")
+  @Returns(200, StrictPageModel)
+  getSuccess() {
+    return {
+      data:
+        [{
+        id: 'foo',
+        name: 'bar'
+      }]
+    }
+  }
+}
